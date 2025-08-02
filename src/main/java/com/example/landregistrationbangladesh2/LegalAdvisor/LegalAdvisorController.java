823a2a179d1dd9167556eaebc9018d9cefb2b94c
rakib <2310387@iub.edu.bd>
2025-08-02 18:45:34 +0600

    initial commit

---
+++ src/main/java/com/example/landregistrationbangladesh2/LegalAdvisor/LegalAdvisorController.java
@@ -0,0 +1,53 @@
+package com.example.landregistrationbangladesh2.LegalAdvisor;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+
+public class LegalAdvisorController
+{
+    @javafx.fxml.FXML
+    private BorderPane legalAdvisorDashboardBP;
+    @javafx.fxml.FXML
+    private TextArea infoShowTA;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void signoutOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void uploadLegalCertificateOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void resolveLandDisputesOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void reviewMutationCasesOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void respondLegalObjectionOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void auditLegalComplianceOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void reviewOwnershipLegalityOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void issueSpecialLegalAdviceOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void legalConsultationComplexOA(ActionEvent actionEvent) {
+    }
+}
